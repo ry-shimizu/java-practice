c3c0243ee2c53fbe5fca566f4d3aa03a2981144f
清水稜也 <ryoya0111g.o.l.f@gmail.com>
2023-02-02 22:41:09 +0900

    メール送信と例外処理についての学習 基礎的な部分については完了

--- java_practice/src/main/java/com/example/demo/Service/MailSenderService.java
+++ java_practice/src/main/java/com/example/demo/Service/MailSenderService.java
@@ -1,33 +1,58 @@
 package com.example.demo.Service;
 
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import javax.xml.crypto.MarshalException;
-import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class MailSenderService {
 
     private final MailSender mailSender;
+    private final JavaMailSender javaMailSender;
 
-    public boolean sendMail() throws Exception {
-        try{
+    public boolean sendMail() {
             var mailInfo = new SimpleMailMessage();
             mailInfo.setSubject("javaの実装練習です");
             mailInfo.setText("お元気ですかテストです");
             // テスト用メールアドレス
             mailInfo.setTo("nagahisawebpage@gmail.com");
             mailInfo.setFrom("ryouya56395639@gmail.com");
+        // 非検査例外のため、try-catch文を書かなくてもよい。
+        // 基本エラーを見れば何が起きたかは分かるが、メッセージをログに残す場合は書いてもよい？
+        try{
             mailSender.send(mailInfo);
             return true;
         } catch (MailException e) {
-            throw new Exception("メール送信に失敗しました", e);
+            throw new RuntimeException("メール送信に失敗しました", e);
+        }
+    }
+
+    public boolean javaMailSender() {
+        // 新しいメッセージを作成
+        var message = javaMailSender.createMimeMessage();
+        try {
+            var messageHelper = new MimeMessageHelper(message, true);
+            messageHelper.setFrom("ryouya56395639@gmail.com");
+            messageHelper.setTo("nagahisawebpage@gmail.com");
+            messageHelper.setText("お元気ですかテストです。JavaMail利用です",
+                    "<span style='color: red'>赤文字出力</span><br>段落下げた");
+            messageHelper.setSubject("タイトルですよ");
+
+            javaMailSender.send(message);
+            return true;
+            // MessagingExceptionは、検査例外なので必ずtry-catchが必要
+        } catch(MessagingException e) {
+            // メール送信失敗で処理を終えるのであれば、throw
+            throw new RuntimeException("メッセージの設定に失敗しました", e);
+            // 終わらせず、処理を続けるのであれば、スタックトレースを残す
+            // e.printStackTrace();
         }
     }
 }
--- java_practice/src/main/java/com/example/demo/controller/MailSenderController.java
+++ java_practice/src/main/java/com/example/demo/controller/MailSenderController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.Response.MailSenderResponse;
 import com.example.demo.Service.MailSenderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +15,17 @@ public class MailSenderController {
    private final MailSenderService mailSenderService;
 
     @PostMapping("/mailSender")
-    public MailSenderResponse MailSend() throws Exception {
+    public ResponseEntity<MailSenderResponse> mailSend() {
         var result = mailSenderService.sendMail();
-        return new MailSenderResponse("success", result);
+        return ResponseEntity.ok().body(new MailSenderResponse("success", result));
+        // どっちでも返却値は同じ 意図的に404返したい時などに利用？
+        // return new MailSenderResponse("success", result);
+    }
+
+    @PostMapping("/javaMailSender")
+    public ResponseEntity<MailSenderResponse> javaMailSender() {
+        var result = mailSenderService.javaMailSender();
+        return ResponseEntity.ok().body(new MailSenderResponse("success", true));
     }
 
 }
